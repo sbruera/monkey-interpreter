076758a1fea090ba7109a5199a50f64f6c7fcc4d
Santiago Bruera <santibruera97@gmail.com>
2024-01-22 00:10:31 -0300

    ch-1.3 - The Lexer

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//Identifiers + literals
+	IDENT = "IDENT" //add, foo, bar, x, y ...
+	INT   = "INT"   // 12345
+
+	//Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//DELIMITERS
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//Keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
